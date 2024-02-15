9a97eee1db76bfc91450d7356d573243768a218c
Janne Venho <134903455+JanneVenhoEdu@users.noreply.github.com>
2024-02-15 13:29:10 +0200

    Create teoriaupotus.js

---
+++ teoriaupotus.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$(".card-title:contains('Teoria')").parent().attr("style", "padding: 0rem!important");
+	$(".card-title:contains('Teoria')").css("padding", "10px");
+});
