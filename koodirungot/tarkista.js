2d9aa6cd292fbd6ac452f1481c809c1e000c40bc
Janne Venho <134903455+JanneVenhoEdu@users.noreply.github.com>
2024-11-01 16:16:32 +0200

    Update tarkista.js

--- koodirungot/tarkista.js
+++ koodirungot/tarkista.js
@@ -1,23 +1,21 @@
-function tarkista(correctAnswers){
-	$(document).ready(function() {
-		$(".tarkista").keydown(function(e) {
-			if (e.key == "Enter" || e.key == "Tab") {
-				var item = +$(this).attr("id");
-				var corr = correctAnswers[item];
-				var user = $(this).val();
-				if (user == corr) {
-					$(this).css("background-color", "#80FF80");
-					$(this).next("span").hide();
-				} else {
-					if (user != "") {
-						$(this).next("span").show();
-						$(this).css("background-color", "#FFCCCC");
-					}
+$(document).ready(function() {
+	$(".tarkista").keydown(function(e) {
+		if (e.key == "Enter" || e.key == "Tab") {
+			var item = +$(this).attr("id");
+			var corr = correctAnswers[item];
+			var user = $(this).val();
+			if (user == corr) {
+				$(this).css("background-color", "#80FF80");
+				$(this).next("span").hide();
+			} else {
+				if (user != "") {
+					$(this).next("span").show();
+					$(this).css("background-color", "#FFCCCC");
 				}
 			}
-		});
-		$(".vihje").click(function() {
-			$(this).parent().next().slideToggle();
-		});
+		}
 	});
-}
+	$(".vihje").click(function() {
+		$(this).parent().next().slideToggle();
+	});
+});
