06d8e97e2e32e79f60a5013ad5971fd236e5ba20
Janne Venho <134903455+JanneVenhoEdu@users.noreply.github.com>
2025-02-14 14:01:06 +0200

    Update tarkista.js

--- tarkista.js
+++ tarkista.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 		if (e.key == "Enter" || e.key == "Tab") {
 			var item = +$(this).attr("id");
 			var corr = correctAnswers[item];
-			var user = $(this).val().replace(\/s\g, "");
+			var user = $(this).val().replace(/\s/g, "");
+			var user = $(this).val().replace(/\s/g, "");
 			if (user == corr) {
 				$(this).css("background-color", "#80FF80");
 				//$(this).next("span").hide();
