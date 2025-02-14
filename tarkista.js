2a592b1b9680689ec3f4b12dd136d9c309fefac0
Janne Venho <134903455+JanneVenhoEdu@users.noreply.github.com>
2025-02-14 12:43:54 +0200

    Create tarkista.js

---
+++ tarkista.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$(".tarkista").keydown(function(e) {
+		if (e.key == "Enter" || e.key == "Tab") {
+			var item = +$(this).attr("id");
+			var corr = correctAnswers[item];
+			var user = $(this).val();
+			if (user == corr) {
+				$(this).css("background-color", "#80FF80");
+				//$(this).next("span").hide();
+			} else {
+				if (user != "") {
+					//$(this).next("span").show();
+					$(this).css("background-color", "#FFCCCC");
+				}
+			}
+		}
+	});
+	$(".vihje").click(function() {
+		$(this).parent().next().slideToggle();
+	});
+	$("#bonus").click(function() {
+		$(this).parent().next().slideToggle();
+		$("#bonus_tarkistus").slideToggle();
+	});
+});
